ecb782f8ec73dec3a5c4d2edcbccc17f49e0cfd8
grace_goose <choieungi@gm.gist.ac.kr>
2021-11-13 14:11:20 +0900

    init

---
+++ src/main/java/com/example/gisteekassignment2/GisteekAssignment2Application.java
@@ -0,0 +1,13 @@
+package com.example.gisteekassignment2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GisteekAssignment2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GisteekAssignment2Application.class, args);
+    }
+
+}
